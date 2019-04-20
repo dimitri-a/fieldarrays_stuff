f200dc3e0db099578e095dda277c66ccc7c31ad8
Dimitri C <couwenberg@gmail.com>
2019-04-20 15:52:16 +1000

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import FormContainer from './Forms/FormContainer';
+import showResults from "./showResults";
+import * as serviceWorker from './serviceWorker';import { Values } from "redux-form-website-template";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(
+    <Provider store={store}>
+      <div style={{ padding: 15 }}>
+        <h2>Selecting Form Values Example</h2>
+        <FormContainer onSubmit={showResults} />
+        <Values form="selectingFormValues" />
+      </div>
+    </Provider>,
+    rootEl
+  );
+  
