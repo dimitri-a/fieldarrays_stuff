21392e4e1595f77dd23bd4660afedee7c3f1b520
Dimitri C <couwenberg@gmail.com>
2019-04-20 19:41:04 +1000

    cleaning

--- src/selectors/sumSelector.js
+++ src/selectors/sumSelector.js
@@ -6,7 +6,6 @@ export const getAmount1 = state => selector(state, "amount1");
 export const getAmount2 = state =>selector(state,"amount2")
 
 export const sumAmounts = state => {
-
     const test = sum(getAmount1(state),getAmount2(state))
     return test;
 };
