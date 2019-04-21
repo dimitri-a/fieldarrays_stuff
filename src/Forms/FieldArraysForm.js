24f77cc0f495c3393ac52c5b64ac04077b2f7aad
Dimitri C <couwenberg@gmail.com>
2019-04-21 12:11:48 +1000

    update

--- src/Forms/FieldArraysForm.js
+++ src/Forms/FieldArraysForm.js
@@ -1,9 +1,13 @@
-import React, { Component, Fragment } from 'react'
-import { Field, FieldArray, reduxForm, getFormValues, change } from 'redux-form'
-import { connect } from 'react-redux'
-import { CalcTotal } from '../calculationHelper';
-import _ from 'lodash';
-
+import React, { Component, Fragment } from "react";
+import {
+  Field,
+  FieldArray,
+  reduxForm,
+  getFormValues,
+  change
+} from "redux-form";
+import { connect } from "react-redux";
+import * as selectors from "../selectors/sumSelector";
 
 const renderField = ({ input, label, type, meta: { touched, error } }) => (
   <div>
@@ -13,107 +17,75 @@ const renderField = ({ input, label, type, meta: { touched, error } }) => (
       {touched && error && <span>{error}</span>}
     </div>
   </div>
-)
+);
 
-const renderMods = ({ fields, meta: { error, submitFailed } }) => (
+const renderitems = ({ fields, meta: { error, submitFailed },t1 }) => (
   <Fragment>
     <ul>
       <li>
         <button type="button" onClick={() => fields.push({})}>
           Add Row
-      </button>
+        </button>
         {submitFailed && error && <span>{error}</span>}
       </li>
-      {fields.map((mod, index) => (
+      {fields.map((item, index) => (
         <li key={index}>
+          1
           <button
             type="button"
             title="Remove Row"
             onClick={() => fields.remove(index)}
           />
           <h4>Rownr #{index + 1}</h4>
-          
           <Field
-            name={`${mod}.amount1`}
+            name={`${item}.amount1`}
             type="number"
             component={renderField}
             label="Amount 1"
           />
-
           <Field
-            name={`${mod}.amount2`}
+            name={`${item}.amount2`}
             type="number"
             component={renderField}
             label="Amount 2"
           />
-        
         </li>
       ))}
     </ul>
 
-    <Field
-      name="totalAmount1"
-      type="number"
-      component="input"
-      label="Total Amount 1"
-    />
+  
+  Total amount1 = {t1}
 
-<Field
+    <Field
       name="totalAmount2"
       type="number"
       component="input"
       label="Total Amount 2"
     />
-
   </Fragment>
-)
+);
 
-
-const isArrayEqual = function (x, y) {
-  return _(x).differenceWith(y, _.isEqual).isEmpty();
-};
 class FieldArraysForm extends Component {
-
-  componentDidMount() {
-    //initialize the total value
-    this.props.change('fieldArraysForm', 'total', 10)
-  }
-
-  // componentDidUpdate(prevProps) {
-  //   let formHasChanged = false
-  //   let newTotal = 0
-
-  //   if (prevProps.formValues && prevProps.formValues.mods && this.props.formValues && this.props.formValues.mods) {
-  //     formHasChanged = !(isArrayEqual(prevProps.formValues.mods, this.props.formValues.mods))
-  //   }
-  //   //form haschanged then calculate the number
-  //   if (formHasChanged) {
-  //     newTotal = CalcTotal(this.props.formValues.mods);
-  //     console.log('calc=', newTotal);
-  //     debugger
-  //     this.props.change("fieldArraysForm", "total", newTotal.currentYear);
-  //   }
-  // }
-
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit } = this.props;
 
     return (
       <form onSubmit={handleSubmit}>
-        <FieldArray name="mods" component={renderMods} />
+        <FieldArray name="items" component={renderitems} />
         <div>
-          <button type="submit" >
-            Submit
-        </button>
+          <button type="submit">Submit</button>
         </div>
       </form>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  formValues: getFormValues('fieldArraysForm')(state),
-});
+const mapStateToProps = state => {
+  return {
+    sum: selectors.sumAmounts(state),
+    // t1: selectors.totalAmount1(state)
+  };
+};
 
 const mapDispatchToProps = {
   change
@@ -127,4 +99,3 @@ export default reduxForm({
     mapDispatchToProps
   )(FieldArraysForm)
 );
-
--- src/selectors/sumSelector.js
+++ src/selectors/sumSelector.js
@@ -1,11 +1,50 @@
 import { sum } from "../calculations";
 import { formValueSelector } from "redux-form";
+import { createSelector } from "reselect";
 const selector = formValueSelector("myForm");
 
 export const getAmount1 = state => selector(state, "amount1");
 export const getAmount2 = state =>selector(state,"amount2")
+export const getAllItems= state =>selector(state,"items")
 
 export const sumAmounts = state => {
     const test = sum(getAmount1(state),getAmount2(state))
     return test;
 };
+
+
+// export const totalAmount1 = createSelector(
+//     getAllItems,
+//     items=>
+//     items.reduce(
+//     (sum,item) => sum +Number(item.amount1),0)
+// )
+
+
+// creates =>{
+//     getAllItems,
+//     getAmount1.reduce(row => row.amount1)
+    
+//     }
+
+// export const getMaxDistributionSum = createSelector(
+//     getShareholderDistribution,
+//     shareholderdistribution =>
+//       shareholderdistribution &&
+//       shareholderdistribution
+//         .filter(s => s.name && s.name === true)
+//         .reduce(
+//           (sum, item) => sum + Number(item.ownership && item.ownership !== 0 ? item.ownership : 0),
+//           0
+//         )
+//   );
+  
+
+
+// export const getAllShareHolders = state =>
+//   state.mockData &&
+//   state.mockData.BusinessFinancials &&
+//   state.mockData.BusinessFinancials.shareHolder &&
+//   state.mockData.BusinessFinancials.shareHolder.all
+//     ? state.mockData.BusinessFinancials.shareHolder.all
+//     : undefined;
