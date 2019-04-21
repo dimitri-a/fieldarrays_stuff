315b40bd18747e355fb757eadf53d2a585ac6eaf
Dimitri C <couwenberg@gmail.com>
2019-04-21 11:28:25 +1000

    updated Fieldarraysform: amount1 and 2

--- src/Forms/FieldArraysForm.js
+++ src/Forms/FieldArraysForm.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { Field, FieldArray, reduxForm, getFormValues, change } from 'redux-form'
 import { connect } from 'react-redux'
-import { CalcTotal } from './calculationHelper';
+import { CalcTotal } from '../calculationHelper';
 import _ from 'lodash';
 
 
@@ -20,7 +20,7 @@ const renderMods = ({ fields, meta: { error, submitFailed } }) => (
     <ul>
       <li>
         <button type="button" onClick={() => fields.push({})}>
-          Add Modification
+          Add Row
       </button>
         {submitFailed && error && <span>{error}</span>}
       </li>
@@ -28,40 +28,41 @@ const renderMods = ({ fields, meta: { error, submitFailed } }) => (
         <li key={index}>
           <button
             type="button"
-            title="Remove Mod"
+            title="Remove Row"
             onClick={() => fields.remove(index)}
           />
-          <h4>Mod #{index + 1}</h4>
+          <h4>Rownr #{index + 1}</h4>
+          
           <Field
-            name={`${mod}.lastYear`}
+            name={`${mod}.amount1`}
             type="number"
             component={renderField}
-            label="Last Year"
+            label="Amount 1"
           />
+
           <Field
-            name={`${mod}.currentYear`}
+            name={`${mod}.amount2`}
             type="number"
             component={renderField}
-            label="Current Year"
+            label="Amount 2"
           />
-
-          <Field name={`${mod}.type`} component="select" label="Type">
-            <option />
-            <option value="-">Expense</option>
-            <option value="+">Income</option>
-            <option value="-">Tax</option>
-          </Field>
-
+        
         </li>
       ))}
     </ul>
 
     <Field
-      name="total"
+      name="totalAmount1"
       type="number"
       component="input"
-      label="Total modifications"
-      text="0"
+      label="Total Amount 1"
+    />
+
+<Field
+      name="totalAmount2"
+      type="number"
+      component="input"
+      label="Total Amount 2"
     />
 
   </Fragment>
@@ -78,21 +79,21 @@ class FieldArraysForm extends Component {
     this.props.change('fieldArraysForm', 'total', 10)
   }
 
-  componentDidUpdate(prevProps) {
-    let formHasChanged = false
-    let newTotal = 0
+  // componentDidUpdate(prevProps) {
+  //   let formHasChanged = false
+  //   let newTotal = 0
 
-    if (prevProps.formValues && prevProps.formValues.mods && this.props.formValues && this.props.formValues.mods) {
-      formHasChanged = !(isArrayEqual(prevProps.formValues.mods, this.props.formValues.mods))
-    }
-    //form haschanged then calculate the number
-    if (formHasChanged) {
-      newTotal = CalcTotal(this.props.formValues.mods);
-      console.log('calc=', newTotal);
-      debugger
-      this.props.change("fieldArraysForm", "total", newTotal.currentYear);
-    }
-  }
+  //   if (prevProps.formValues && prevProps.formValues.mods && this.props.formValues && this.props.formValues.mods) {
+  //     formHasChanged = !(isArrayEqual(prevProps.formValues.mods, this.props.formValues.mods))
+  //   }
+  //   //form haschanged then calculate the number
+  //   if (formHasChanged) {
+  //     newTotal = CalcTotal(this.props.formValues.mods);
+  //     console.log('calc=', newTotal);
+  //     debugger
+  //     this.props.change("fieldArraysForm", "total", newTotal.currentYear);
+  //   }
+  // }
 
   render() {
     const { handleSubmit } = this.props
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import FormContainer from "./Forms/FormContainer";
+// eslint-disable-next-line no-unused-vars
+import FormContainer from "./Forms/FormContainer";
 import showResults from "./showResults";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
 import store from "./store";
 
-import FormMain from './FieldArraysForm'
+import FormMain from './Forms/FieldArraysForm'
 serviceWorker.unregister();
 
 const rootEl = document.getElementById("root");
